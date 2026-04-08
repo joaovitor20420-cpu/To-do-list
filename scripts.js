d0b05c87af25c0ecc90354088881fc10cc038ca3
Developer <dev@example.com>
2026-04-07 22:17:29 -0300

    feat: implement task persistence using localStorage and add task removal functionality

--- scripts.js
+++ scripts.js
@@ -1,6 +1,55 @@
 const inputTask = document.querySelector("#input-task");
 const btnAddTask = document.querySelector("#btn-add");
 const taskContainer = document.querySelector("#task-container"); 
+const buttonRemove = document.querySelector("#button-remove");
+const tasksJson = localStorage.getItem("myTasks");
+const taskReverse = JSON.parse(tasksJson);
+
+let myTasks = JSON.parse(localStorage.getItem("myTasks")) || [];
+taskReverse.forEach(element => {
+    const taskElement = document.createElement('p');
+    taskElement.classList.add('task');
+    taskElement.textContent = element;
+    taskContainer.appendChild(taskElement);
+    
+    const btnConcluir = document.createElement('button');
+    btnConcluir.textContent = "Concluir";
+    btnConcluir.classList.add('btn-concluir');
+    taskElement.appendChild(btnConcluir);
+
+    btnConcluir.addEventListener("click", () => {
+        btnConcluir.remove();
+        
+        const emoji = document.createElement('span');
+        emoji.textContent = "✅";
+        taskElement.appendChild(emoji);
+    });
+    const btnExcluir = document.createElement('button');
+
+    btnExcluir.textContent = "Excluir";
+    btnExcluir.classList.add('btn-excluir');
+    taskElement.appendChild(btnExcluir);
+
+    taskElement.addEventListener("click", (e) => {
+        e.stopPropagation();
+        btnExcluir.style.display = "flex";
+        btnExcluir.style.backgroundColor = "#8b0606";
+        btnConcluir.style.display = "none";
+        
+
+        btnExcluir.addEventListener("click", () => {
+            taskElement.remove();
+        });
+        
+        
+    });
+
+    taskContainer.addEventListener("click", () => {
+        btnExcluir.style.display = "none";
+        btnConcluir.style.display = "flex";
+    });
+});
+
 
 btnAddTask.addEventListener("click", () => {
     const taskText = inputTask.value.trim();
@@ -38,6 +87,12 @@ btnAddTask.addEventListener("click", () => {
 
             btnExcluir.addEventListener("click", () => {
                 taskElement.remove();
+                const index = myTasks.indexOf(taskText);
+                if(index !== -1) {
+                    myTasks.splice(index, 1);
+                }
+                const tasksJson = JSON.stringify(myTasks);
+                localStorage.setItem("myTasks", tasksJson);
             });
             
             
@@ -48,11 +103,14 @@ btnAddTask.addEventListener("click", () => {
             btnConcluir.style.display = "flex";
         });
 
-
+        myTasks.push(taskText);
+        const tasksJson = JSON.stringify(myTasks);
+        localStorage.setItem("myTasks", tasksJson);
 
     } else {
         alert("Por favor, insira uma tarefa.");
     }
 });
+
 
 
